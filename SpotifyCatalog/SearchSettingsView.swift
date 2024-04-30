d9a925940d3bbcff94cf110a876f1128a35ac7c6
Scott Crowder <scrowder@gmail.com>
2024-04-30 17:29:16 -0500

    build settings ui

--- SpotifyCatalog/SearchSettingsView.swift
+++ SpotifyCatalog/SearchSettingsView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct SearchSettingsView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding var selectedMarket: String
+    @Binding var searchTypes: [AudioType]
+    
+    let adaptiveLayout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Market") {
+                    Picker("Market", selection: $selectedMarket) {
+                        ForEach(Market.knownMarkets) { market in
+                            Text(market.country)
+                                .tag(market.id)
+                        }
+                    }
+                }
+                
+                Section("Search For") {
+                    LazyVGrid(columns: adaptiveLayout) {
+                        ForEach($searchTypes) { $audioType in
+                            HStack{
+                                Image(systemName: audioType.isChecked ? "checkmark.square" : "square")
+                                Text(audioType.name)
+                            }
+                            .font(.caption)
+                            .onTapGesture {
+                                audioType.isChecked.toggle()
+                            }
+                        }
+                    }
+                }
+                
+                
+            }
+            .navigationTitle("Search Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SearchSettingsView()
+    SearchSettingsView(selectedMarket: .constant("US"), searchTypes: .constant([]))
 }
