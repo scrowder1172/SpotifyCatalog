9cde97d9d1841eff13448750bf618e0cb6c6d79c
Scott Crowder <scrowder@gmail.com>
2024-04-30 10:27:13 -0500

    Initial Commit

---
+++ SpotifyCatalog/SpotifyCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCatalogApp.swift
+//  SpotifyCatalog
+//
+//  Created by SCOTT CROWDER on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
