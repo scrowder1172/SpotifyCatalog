5f9381173b812793b28ae063b05e01d571c73b07
Scott Crowder <scrowder@gmail.com>
2024-04-30 15:25:11 -0500

    add view for search settings

---
+++ SpotifyCatalog/SearchSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchSettingsView.swift
+//  SpotifyCatalog
+//
+//  Created by SCOTT CROWDER on 4/30/24.
+//
+
+import SwiftUI
+
+struct SearchSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchSettingsView()
+}
